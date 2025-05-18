ddb0b52604bff009759c581ce290c92b7f64c54a
Marshall-James-22509909 <146729658+Marshall-James-22509909@users.noreply.github.com>
2025-05-18 01:15:26 +0100

    Created a rotating cube
    Used the code from week 6s lab to create a rotating cube with a texture. This will be the baseline that I continue the project from.

--- source/coursework.cpp
+++ source/coursework.cpp
@@ -14,6 +14,9 @@
 // Function prototypes
 void keyboardInput(GLFWwindow *window);
 
+// Create camera object
+Camera camera(glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0.0f, 0.0f, -2.0f));
+
 int main( void )
 {
     // =========================================================================
@@ -57,25 +60,210 @@ int main( void )
     // -------------------------------------------------------------------------
     // End of window creation
     // =========================================================================
+
+     // Enable depth test
+    glEnable(GL_DEPTH_TEST);
     
     // Ensure we can capture keyboard inputs
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // Define cube object
+    // Define vertices
+    const float vertices[] = {
+        // front
+        -1.0f, -1.0f,  1.0f,    //              + ------ +
+         1.0f, -1.0f,  1.0f,    //             /|       /|
+         1.0f,  1.0f,  1.0f,    //   y        / |      / |
+        -1.0f, -1.0f,  1.0f,    //   |       + ------ +  |
+         1.0f,  1.0f,  1.0f,    //   + - x   |  + ----|- +
+        -1.0f,  1.0f,  1.0f,    //  /        | /      | /
+        // right                // z         |/       |/
+         1.0f, -1.0f,  1.0f,    //           + ------ +
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+         // back
+          1.0f, -1.0f, -1.0f,
+         -1.0f, -1.0f, -1.0f,
+         -1.0f,  1.0f, -1.0f,
+          1.0f, -1.0f, -1.0f,
+         -1.0f,  1.0f, -1.0f,
+          1.0f,  1.0f, -1.0f,
+          // left
+          -1.0f, -1.0f, -1.0f,
+          -1.0f, -1.0f,  1.0f,
+          -1.0f,  1.0f,  1.0f,
+          -1.0f, -1.0f, -1.0f,
+          -1.0f,  1.0f,  1.0f,
+          -1.0f,  1.0f, -1.0f,
+          // bottom
+          -1.0f, -1.0f, -1.0f,
+           1.0f, -1.0f, -1.0f,
+           1.0f, -1.0f,  1.0f,
+          -1.0f, -1.0f, -1.0f,
+           1.0f, -1.0f,  1.0f,
+          -1.0f, -1.0f,  1.0f,
+          // top
+          -1.0f,  1.0f,  1.0f,
+           1.0f,  1.0f,  1.0f,
+           1.0f,  1.0f, -1.0f,
+          -1.0f,  1.0f,  1.0f,
+           1.0f,  1.0f, -1.0f,
+          -1.0f,  1.0f, -1.0f,
+    };
+
+    // Define texture coordinates
+    const float uv[] = {
+        // front
+        0.0f, 0.0f,     // vertex co-ordinates are the same for each side
+        1.0f, 0.0f,     // of the cube so repeat every six vertices
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        // right
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        // back
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        // left
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        // bottom
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        // top
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+    };
+
+    // Define indices
+    unsigned int indices[] = {
+        0,   1,  2,  3,  4,  5,     // front
+        6,   7,  8,  9, 10, 11,     // right
+        12, 13, 14, 15, 16, 17,     // back
+        18, 19, 20, 21, 22, 23,     // left
+        24, 25, 26, 27, 28, 29,     // bottom
+        30, 31, 32, 33, 34, 35      // top
+    };
+
+    // Create the Vertex Array Object (VAO)
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Create Vertex Buffer Object (VBO)
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create texture buffer
+    unsigned int uvBuffer;
+    glGenBuffers(1, &uvBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(uv), uv, GL_STATIC_DRAW);
+
+    // Create Element Buffer Object (EBO)
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Compile shader program
+    unsigned int shaderID;
+    shaderID = LoadShaders("vertexShader.glsl", "fragmentShader.glsl");
+
+    // Load the textures
+    unsigned int texture;
+    texture = loadTexture("../assets/crate.jpg");
+
+    // Send the texture uniforms to the fragment shader
+    glUseProgram(shaderID);
+    unsigned int textureID;
+    textureID = glGetUniformLocation(shaderID, "texture");
+    glUniform1i(textureID, 0);
 
     // Render loop
     while (!glfwWindowShouldClose(window))
     {
         // Get inputs
         keyboardInput(window);
-        
+
         // Clear the window
         glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Send the VBO to the GPU
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+        // Send the UV buffer to the GPU
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+        // Calculate the model matrix
+        float angle = Maths::radians(glfwGetTime() * 360.0f / 3.0f);
+        glm::mat4 translate = Maths::translate(glm::vec3(0.0f, 0.0f, -2.0f));
+        glm::mat4 scale = Maths::scale(glm::vec3(0.5f, 0.5f, 0.5f));
+        glm::mat4 rotate = Maths::rotate(angle, glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 model = translate * rotate * scale;
+
+        // Calculate the view matrix
+        glm::mat4 view = glm::lookAt(glm::vec3(1.0f, 1.0f, 1.0f),  // eye
+            glm::vec3(0.0f, 0.0f, -2.0f), // target
+            glm::vec3(0.0f, 1.0f, 0.0f)); // worldUp
+
+        // Calculate orthographic projection matrix
+        glm::mat4 projection = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, 0.0f, 10.0f);
+
+        // Send MVP matrix to the vertex shader
+        glm::mat4 MVP = projection * view * model;
+        glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
+
+        // Draw the triangles
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+
         // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+    // Cleanup
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &uvBuffer);
+    glDeleteProgram(shaderID);
+
     // Close OpenGL window and terminate GLFW
     glfwTerminate();
     return 0;
